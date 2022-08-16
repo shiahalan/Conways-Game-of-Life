ee06a28f068735cc3996bb13efdb26a9ef848b33
Alan Shiah <shiahbusiness@gmail.com>
2022-08-15 23:05:18 -0700

    Updated display to create cell

--- conway.py
+++ conway.py
@@ -9,25 +9,29 @@ WINDOW = pygame.display.set_mode((WIN))
 
 def grid():
 
-  def draw(width, cell_height, cell_width, window, rows):
+  def draw(width, cell_height, cell_width, surface, rows):
     x, y = 0, 0
 
     for i in range(rows):
       x += cell_width
       y += cell_height
-      pygame.draw.line(window, (100, 100, 100), (x, 0), (x, width))
-      pygame.draw.line(window, (100, 100, 100), (0, y), (width, y))
+      pygame.draw.line(surface, (100, 100, 100), (x, 0), (x, width))
+      pygame.draw.line(surface, (100, 100, 100), (0, y), (width, y))
 
   
   while True:
     draw(WIDTH, HEIGHT/ROWS, WIDTH/ROWS, WINDOW, ROWS)
+    draw_cell(WINDOW)
 
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         pygame.quit()
         sys.exit()
     
-    pygame.display.update()
+    pygame.display.flip()  # Flip changed to update
 
+
+def draw_cell(surface):
+  pygame.draw.rect(surface, (54, 115, 64), (5, 5, 5, 5))
 
 grid()
